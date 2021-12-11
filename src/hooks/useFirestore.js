96e79a2b14ec1b377f04f4be8e5eb09430049334
nouria chaouy <nouria.chaouy@gmail.com>
2021-12-11 10:04:27 +0000

    completed Firestore hook and added the delete logic to the transactions

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -11,11 +11,13 @@ let initialState = {
 const firestoreReducer = (state, action) => {
     switch (action.type) {
         case 'IS_PENDING':
-            return {isPending: true, document: null, success: false, error: null}
+            return { isPending: true, document: null, success: false, error: null }
         case 'ADDED_DOCUMENT':
-            return {isPending: false, document: action.payload, success: true, error: null }
-            case 'ERROR':
-                return {isPending: false, document: null, success: false, error: action.payload}
+            return { isPending: false, document: action.payload, success: true, error: null }
+        case 'ERROR':
+            return { isPending: false, document: null, success: false, error: action.payload }
+        case 'DELETED_DOCUMENT':
+            return { isPending: false, document: null, success: true, error: null }
         default:
             return state
     }
@@ -53,6 +55,15 @@ export const useFirestore = (collection) => {
 
     // delete a document
     const deleteDocument = async (id) => {
+        dispatch({type: 'PENDING'})
+
+        try {
+           await ref.doc(id).delete()
+           dispatchIfNotCancelled({ type: 'DELETED_DOCUMENT' })
+        } 
+        catch(err) {
+           dispatchIfNotCancelled({ type: 'ERROR', payload: 'Could not delete !' })
+        }
 
     }
 
--- src/pages/home/TransactionList.js
+++ src/pages/home/TransactionList.js
@@ -1,6 +1,10 @@
 import styles from './Home.module.css';
+import { useFirestore } from '../../hooks/useFirestore';
 
 export default function TransactionList({transactions}) {
+    const { deleteDocument, response } = useFirestore('transactions');
+    console.log(response);
+    
     let sum = 0;
     transactions.forEach((transaction) => {
         let numTransac = parseInt(transaction.amount);
@@ -13,6 +17,7 @@ export default function TransactionList({transactions}) {
              <li key={transaction.id}>
                  <p className={styles.name}>{transaction.name}</p>
                  <p className={styles.amount}>${transaction.amount}</p>
+                 <button onClick={() => deleteDocument(transaction.id)}>x</button>
             </li>
          ))}
           <li className={styles.total}>
